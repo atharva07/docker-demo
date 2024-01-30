80e0a74fce7f9856691d95724ae878128bc42a9a
atharva07 <atharva.hiwase07@gmail.com>
2024-01-30 13:04:47 +0530

    login page added

--- 03-automation-framework/selenium-docker/src/main/java/com/atharvahiwase07/pages/vendorPortal/LoginPage.java
+++ 03-automation-framework/selenium-docker/src/main/java/com/atharvahiwase07/pages/vendorPortal/LoginPage.java
@@ -1,18 +1,38 @@
 package com.atharvahiwase07.pages.vendorPortal;
 
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import com.atharvahiwase07.pages.AbstractPage;
 
 public class LoginPage extends AbstractPage {
+    
+    @FindBy(id = "username")
+    private WebElement userName;
+
+    @FindBy(id = "password")
+    private WebElement passWord;
+
+    @FindBy(id = "login")
+    private WebElement loginButton;
+
     public LoginPage(WebDriver driver) {
         super(driver);
     }
 
     @Override
     public boolean isAt(){
-        return false;
+        this.wait.until(ExpectedConditions.visibilityOf(this.userName));
+        return this.userName.isDisplayed();
     }
 
-    
+    public void loginPage(String username, String password) {
+        this.userName.sendKeys(username);
+        this.passWord.sendKeys(password);
+    }    
+
+    public void login() {
+        this.loginButton.click();
+    }
 }
