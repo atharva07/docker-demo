6985edec367b930995c7f4c1feb15e837381cf4d
atharva07 <atharva.hiwase07@gmail.com>
2024-02-04 15:43:38 +0530

    major changes regarding selenium grid integration

--- 03-automation-framework/selenium-docker/src/test/java/com/atharvahiwase07/tests/AbstractTest.java
+++ 03-automation-framework/selenium-docker/src/test/java/com/atharvahiwase07/tests/AbstractTest.java
@@ -1,18 +1,45 @@
 package com.atharvahiwase07.tests;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public abstract class AbstractTest {
     protected WebDriver driver;
 
     @BeforeTest
-    public void setDriver(){
+    public void setDriver() throws MalformedURLException {
+        if(Boolean.getBoolean("selenium.grid.enabled"))
+        {
+            this.driver = getRemoteDriver();
+        } else {
+            this.driver = getLocalDriver();
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private WebDriver getRemoteDriver() throws MalformedURLException {
+        Capabilities capabilities;
+        if(System.getProperty("browser").equalsIgnoreCase("chrome"))
+        {
+            capabilities = new ChromeOptions();
+        } else {
+            capabilities = new FirefoxOptions();
+        }
+        return new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+    }
+
+    private WebDriver getLocalDriver() {
         WebDriverManager.chromedriver().setup();
-        this.driver = new ChromeDriver();
+        return new ChromeDriver();
     }
 
     @AfterTest
--- 04-selenium-grid/selenium-docker/src/main/java/com/atharvahiwase07/pages/flightregistration/Main.java
+++
@@ -1,7 +0,0 @@
-package com.atharvahiwase07.pages.flightregistration;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
