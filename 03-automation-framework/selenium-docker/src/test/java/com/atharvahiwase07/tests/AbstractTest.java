872fbe1ccedcc5043c2df4702ffee0646497800a
atharva07 <atharva.hiwase07@gmail.com>
2024-02-07 02:37:21 +0530

    testng failure screenshot added

--- 03-automation-framework/selenium-docker/src/test/java/com/atharvahiwase07/tests/AbstractTest.java
+++ 03-automation-framework/selenium-docker/src/test/java/com/atharvahiwase07/tests/AbstractTest.java
@@ -1,5 +1,4 @@
 package com.atharvahiwase07.tests;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
@@ -9,11 +8,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 
+import com.atharvahiwase07.listener.TestListener;
 import com.atharvahiwase07.utils.Config;
 import com.atharvahiwase07.utils.Constants;
 import com.google.common.util.concurrent.Uninterruptibles;
@@ -23,6 +25,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Listeners({TestListener.class})
 public abstract class AbstractTest {
 
     private static final Logger log = LoggerFactory.getLogger(AbstractTest.class);
@@ -34,12 +37,14 @@ public abstract class AbstractTest {
     }
 
     @BeforeTest
-    public void setDriver() throws MalformedURLException {
+    public void setDriver(ITestContext ctx) throws MalformedURLException {
         if(Boolean.parseBoolean(Config.get(Constants.GRID_ENABLED)))
         {
             this.driver = getRemoteDriver();
+            ctx.setAttribute(Constants.DRIVER, this.driver);
         } else {
             this.driver = getLocalDriver();
+            ctx.setAttribute(Constants.DRIVER, this.driver);
         }
     }
 
